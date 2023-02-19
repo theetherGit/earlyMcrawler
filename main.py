2719e80b7283a8ef344cf3a3d1a4f7c9e7e6a10b
Shivam Meena <meenashivam9650@gmail.com>
2023-02-20 02:37:57 +0530

    Test 2

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 import asyncio
+import json
+
 import aiohttp
 import aioredis
 from datetime import datetime
-from prisma import Client
+from prisma import Prisma
 from dotenv import load_dotenv
 from os import getenv
 
 load_dotenv()
 
 api_client = None
+redis_client = None
+db = None
 
 
 async def get_manga(new_manga):
     print('Manga', new_manga)
-    async with api_client.get('/api/manga/'+new_manga['slug']) as manga_response:
+    async with api_client.get('/api/manga/' + new_manga['slug']) as manga_response:
         new_manga_data = await manga_response.json()
         print(new_manga_data)
 
 
 async def main():
-    global api_client
+    global api_client, redis_client, db
     async with aiohttp.ClientSession('https://earlym.org') as session:
         api_client = session
-        redis = await aioredis.from_url('redis://localhost:6379')
-        # prisma = Client()
+        redis_client = await aioredis.from_url('redis://localhost:6379')
+        db = Prisma()
+        await db.connect()
 
         async with api_client.get('/api/home') as response:
             data = await response.json()
@@ -32,10 +37,13 @@ async def main():
             for new_manga in latest_updates:
                 print('Manga', new_manga)
                 await get_manga(new_manga)
+                await db.newmanga.create(
+                    data={'slug': new_manga['slug']}
+                )
 
-        await redis.close()
+        await redis_client.close()
         await session.close()
-        # await prisma.disconnect()
+        await db.disconnect()
 
 
 asyncio.run(main())
